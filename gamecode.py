c32d67c9b09c940c2264a9da74ae116d3b62f89b
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2022-12-09 14:20:27 +0530

    Update gamecode.py

--- gamecode.py
+++ gamecode.py
@@ -5,7 +5,7 @@ def findMissingNumbers(n):
     for i in range(1, n[-1]):
         if i not in numbers:
             output.append(i)
-    return output
+    print(output)
     
-listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
-print(findMissingNumbers(listOfNumbers))
+l = list(eval(input("enter more than one numbers seperated by ','")))
+findMissingNumbers(l)
